993c771bea26a7aaa8d862601ab54b2c3367b2c0
tcc5102 <tyler.cooper91@gmail.com>
2016-07-19 16:13:14 -0400

    AngularAsmt-9 Complete

--- dist/scripts/directives/seekBar.js
+++ dist/scripts/directives/seekBar.js
@@ -32,6 +32,10 @@
           return {width: percentString()};
         };
 
+        scope.thumbStyle = function() {
+          return {left: percentString()};
+        };
+
         scope.onClickSeekBar = function(event) {
           var percent = calculatePercent(seekBar, event);
           scope.value = percent * scope.max;
@@ -50,7 +54,6 @@
             $document.unbind('mouseup.thumb');
           });
         };
-
       }
     };
   }
